536f4b79b8a6d436134476d8d6532428aacd2742
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-10 14:47:28 +0200

    File editing test

--- Ch5/k5_q2.c
+++ Ch5/k5_q2.c
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc,char *argv) {
-    
+int main(int argc,char *argv[]) {
+    close(STDOUT_FILENO);
+    open("./k5_q2.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+    printf("Hello");
+    close(k5_q2.output);
 }
