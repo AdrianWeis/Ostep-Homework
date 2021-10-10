fe4f3d7ea2f9ea408539865143f0ef6db11b5c4c
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-10 14:52:45 +0200

    file finally?

--- Ch5/k5_q2.c
+++ Ch5/k5_q2.c
@@ -7,7 +7,6 @@
 
 int main(int argc,char *argv[]) {
     close(STDOUT_FILENO);
-    open("./k5_q2.output", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
+    open("./k5_q2.output", O_CREAT|O_WRONLY|O_TRUNC);
     printf("Hello");
-    close(k5_q2.output);
 }
