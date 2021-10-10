fcd522ed1331469aaaf0e06736887b0fe4393449
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-10 18:13:35 +0200

    txt test

--- Ch5/k5_q2.c
+++ Ch5/k5_q2.c
@@ -7,6 +7,16 @@
 
 int main(int argc,char *argv[]) {
     close(STDOUT_FILENO);
-    open("./k5_q2.output", O_CREAT|O_WRONLY|O_TRUNC);
-    printf("Hello");
+    open("./k5_q2.txt", O_CREAT|O_WRONLY|O_TRUNC);
+    int rc = fork();
+    if(rc < 0) {
+        fprintf(stderr, "Fork failed\n");
+        exit(1);
+    } else if (rc == 0)
+    {
+        printf("Hello");
+    } else
+    {
+        printf(" and Goodby");
+    }
 }
