3d4797cc102a8f4685f06129facfbdc9544efaba
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-10 18:51:29 +0200

    Cheat in q3

--- Ch5/k5_q3.c
+++ Ch5/k5_q3.c
@@ -2,19 +2,53 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/time.h>
 
 int main(){
+    FILE *fp;
+    int i;
+
+    fp = fopen("cheat.txt", "w");
+
+    if(fp == NULL) {
+        printf("Datei konnte nicht geoeffnet werden.\n");
+    }else {
+        fputc(1, fp);
+        
+        fclose(fp);
+    }
+
     int rc = fork();
     if(rc < 0) {
         fprintf(stderr, "Fork failed\n");
         exit(1);
     } else if (rc == 0)
     {
+        fp = fopen("cheat.txt", "w");
+
+        if(fp == NULL) {
+            printf("Datei konnte nicht geoeffnet werden.\n");
+        }else {
+            fputc(0, fp);
+            
+            fclose(fp);
+        }
         printf("Hello\n");
     } else
     {
-        spin();
+        bool *loop;
+        *loop = 1;
+        while(*loop == 1)
+        {
+            fp = fopen("cheat.txt", "r");
+
+            if(fp == NULL) {
+                printf("Datei konnte nicht geoeffnet werden.\n");
+            }else {
+                // komplette Datei zeichenweise ausgeben
+                *loop = (fgetc(fp) == '1');
+                fclose(fp);
+            }
+        }
         printf("Goodby\n");
     }
 }
