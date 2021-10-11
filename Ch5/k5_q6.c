58e02ac6de1839f70fd0b72bcee395cf2cc85596
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-11 11:56:55 +0200

    Heyho guter flow

--- Ch5/k5_q6.c
+++ Ch5/k5_q6.c
@@ -11,13 +11,12 @@ int main() {
         exit(1);
     } else if (rc == 0)
     {
-        //int rc_wait = 
-        wait(NULL);
+        //int rc_wait = wait(NULL);
         //printf("Return of wait:%d",rc_wait);
         printf("Hello");
     } else
     {
-        int rc_wait = wait(NULL);
+        int rc_wait = waitpid(rc,WIFEXITED,WNOHANG);
         printf(" and Goodby\n");
         fprintf("Return of wait:%d",rc_wait);
     }
