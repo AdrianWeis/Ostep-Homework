4605f4edd8a9af8e27012a59a8f955d5bfaabf52
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-11 11:02:39 +0200

    besserer vergleich?

--- Ch5/k5_q4.c
+++ Ch5/k5_q4.c
@@ -10,8 +10,10 @@ extern char **environ;
 int main(int argc, char*argv[]) {
 
     if (argc != 2){
-        printf("Fehlerhafteraufruf: k5_q4 int");
+        printf("Fehlerhafteraufruf: k5_q4 int\n");
     }
+
+    char *p;
 
     int rc = fork();
 
@@ -21,42 +23,42 @@ int main(int argc, char*argv[]) {
     }
     else if (rc == 0)
     {
-        if (strncmp(argv[1],"1", strlen(argv[1])))
+        if (strtol(argv[1], &p, 10)==1)
         {
             char* myargs[2];
             myargs[0] = strdup("ls");
             myargs[1] = NULL;
             execvp(myargs[0],myargs);
         }
-        else if (strncmp(argv[1],"2", strlen(argv[1])))
+        else if (strtol(argv[1], &p, 10)==2)
         {
             char* myargs[2];
             myargs[0] = strdup("ls");
             myargs[1] = NULL;
             execvpe(myargs[0],myargs,environ);
         }
-        else if (strncmp(argv[1],"3", strlen(argv[1])))
+        else if (strtol(argv[1], &p, 10)==3)
         {
             char* myargs[2];
             myargs[0] = strdup("/bin/ls");
             myargs[1] = NULL;
             execv(myargs[0],myargs);
         }
-        else if (strncmp(argv[1],"4", strlen(argv[1])))
+        else if (strtol(argv[1], &p, 10)==4)
         {
             char* myargs[2];
             myargs[0] = strdup("/bin/ls");
             myargs[1] = NULL;
             execl(myargs[0],myargs[1]);
         }
-        else if (strncmp(argv[1],"5", strlen(argv[1])))
+        else if (strtol(argv[1], &p, 10)==5)
         {
             char* myargs[2];
             myargs[0] = strdup("/bin/ls");
             myargs[1] = NULL;
             execle(myargs[0],myargs[1],NULL);
         }
-        else if (strncmp(argv[1],"0", strlen(argv[1])))
+        else if (strtol(argv[1], &p, 10)==0)
         {
             char* myargs[2];
             myargs[0] = strdup("ls");
