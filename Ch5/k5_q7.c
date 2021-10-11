b0410ba8c596b9c10550bd6e86d85cf333994c63
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-11 12:14:29 +0200

    q7 test

---
+++ Ch5/k5_q7.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    int wstatus;
+    int rc = fork();
+    if(rc < 0) {
+        fprintf(stderr, "Fork failed\n");
+        exit(1);
+    } else if (rc == 0)
+    {
+        close(STDOUT_FILENO);
+        printf("Hello\n");
+    } else
+    {
+       
+    }
+    return(0);
+}
