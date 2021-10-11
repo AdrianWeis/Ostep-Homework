83375326f00377624d78ebc445be6f76a3205756
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-11 18:37:30 +0200

    mit leerem read

--- Ch6/messung_sys.c
+++ Ch6/messung_sys.c
@@ -10,11 +10,22 @@ int main()
     clock_gettime(CLOCK_MONOTONIC_RAW,&loopStart);
     for(int i = 0; i < iterations; i++)
     {
-
+        
     }
     clock_gettime(CLOCK_MONOTONIC_RAW,&loopEnd);
 
-    long double avrLoopTime = (loopStart.tv_nsec - loopEnd.tv_nsec)/iterations;
+    long double avrLoopTime = ((long double) loopStart.tv_nsec - (long double) loopEnd.tv_nsec)/(double) iterations;
+    
+    clock_gettime(CLOCK_MONOTONIC_RAW,&clockStart);
+    for(int i = 0; i < iterations; i++)
+    {
+        read(0,NULL,0);
+    }
+    clock_gettime(CLOCK_MONOTONIC_RAW,&clockEnd);
+
+    long double avrRead = ((long double) clockStart.tv_nsec - (long double) clockEnd.tv_nsec)/(long double) iterations;
     printf("Durchschnittliche Dauer einer Schleife in: %Lf nsec\n", avrLoopTime);
+    printf("Durchschnittliche Dauer eines 0ByteReads in: %Lf nsec\n", avrRead);
+
     return 0;
 }
