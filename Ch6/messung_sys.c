a50d72756c166e250dc34e0b7a710edb0e477300
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-11 20:00:51 +0200

    know your struct

--- Ch6/messung_sys.c
+++ Ch6/messung_sys.c
@@ -7,6 +7,7 @@ int main()
 {
     struct timespec loopStart,loopEnd,clockStart,clockEnd;
     int iterations = 30000000; //10^9 war zu viel...
+    int secToNs = 1000000000;
 
     clock_gettime(CLOCK_MONOTONIC_RAW,&loopStart);
     for(int i = 0; i < iterations; i++)
@@ -14,7 +15,7 @@ int main()
     }
     clock_gettime(CLOCK_MONOTONIC_RAW,&loopEnd);
 
-    long double avrLoopTime = ((long double) loopEnd.tv_nsec - (long double) loopStart.tv_nsec)/(long double) iterations;
+    long double avrLoopTime = ((long double) (loopEnd.tv_sec * secToNs + loopEnd.tv_nsec) - (long double) (loopStart.tv_sec * secToNs + loopStart.tv_nsec)/(long double) iterations;
     
     clock_gettime(CLOCK_MONOTONIC_RAW,&clockStart);
     for(int i = 0; i < iterations; i++)
@@ -23,7 +24,7 @@ int main()
     }
     clock_gettime(CLOCK_MONOTONIC_RAW,&clockEnd);
 
-    long double avrRead = ((long double) clockEnd.tv_nsec - (long double) clockStart.tv_nsec)/(long double) iterations;
+    long double avrRead = ((long double) (clockEnd.tv_sec * secToNs + clockEnd.tv_nsec) - (long double) (clockStart.tv_sec * secToNs + clockStart.tv_nsec)/(long double) iterations;
     avrRead = avrRead - avrLoopTime;
     
     printf("Durchschnittliche Dauer einer Schleife in: %Lf nsec\n", avrLoopTime);
