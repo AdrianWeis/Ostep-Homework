da8feec848bdef8374742de3539cf9f952f9e716
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-12 12:18:53 +0200

    mit der schleife hab ichs kaput gmacht

--- Ch5/k5_q2.c
+++ Ch5/k5_q2.c
@@ -6,20 +6,22 @@
 #include <sys/wait.h>
 
 int main() {
-    close(STDOUT_FILENO);
-    open("./k5_q2.output", O_CREAT|O_WRONLY|O_TRUNC);
+
+    int fd = open("./k5_q2.txt", O_CREAT|O_WRONLY|O_TRUNC);
     int rc = fork();
     if(rc < 0) {
         fprintf(stderr, "Fork failed\n");
         exit(1);
     } else if (rc == 0)
     {
+        dup2(fd,STDOUT_FILENO);
         for (int i; i < 100; i++)
         {
             printf("AaAa");
         }
     } else
     {
+        dup2(fd,STDOUT_FILENO);
         for (int n; n < 100; n++)
         {
             printf("BbBb");
