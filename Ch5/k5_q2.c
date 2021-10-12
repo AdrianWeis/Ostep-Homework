ed6e59d718f3f8fb5d24bcc379260b71349b47eb
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-12 12:27:52 +0200

    jetzt mit write

--- Ch5/k5_q2.c
+++ Ch5/k5_q2.c
@@ -14,17 +14,18 @@ int main() {
         exit(1);
     } else if (rc == 0)
     {
-        dup2(fd,STDOUT_FILENO);
+        char ** a = "AaAa";
         for (int i; i < 100; i++)
         {
-            printf("AaAa");
+            write(fd,b,4);
         }
     } else
     {
-        dup2(fd,STDOUT_FILENO);
+        //dup2(fd,STDOUT_FILENO);
+        char ** b = "BbBb";
         for (int n; n < 100; n++)
         {
-            printf("BbBb");
+            write(fd,b,4);
         }
     }
     return(0);
