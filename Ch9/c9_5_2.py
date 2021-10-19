f89b48781d82d875de084ef4d0af2a1125e4e94d
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-19 13:21:55 +0200

    versuch 2

---
+++ Ch9/c9_5_2.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Data for plotting
+t = np.arange(0.0, 1.0, 0.1)
+s = np.arange(0.0, 1000.0, 1.0)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel='length of Jobs', ylabel='fairness',
+       title='Test')
+ax.grid()
+
+fig.savefig("Figur.png")
+plt.show()
