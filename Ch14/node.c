f8b8c08b5ab6678127dec187638d9b171238d337
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-26 13:45:22 +0200

    dgd

--- Ch14/node.c
+++ Ch14/node.c
@@ -54,9 +54,9 @@ void printDaten(struct daten *pointer)
 int main()
 {
     struct daten pre = {(int*) malloc(sizeof(int)),0}
-    struct daten v1 = &pre;
+    struct daten v1 = pre;
     add(&v1,1);
-    struct daten v2 = &pre;
+    struct daten v2 = pre;
     add(&v2,2);
     add(&v2,3);
     struct daten array[] = {v1,v2};
