045e1265e6047cc1d285180a808b2dac2107f521
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-26 08:40:13 +0200

    add array test

---
+++ Ch14/array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+int main()
+{
+    int* p;
+    p = malloc(sizeof(int)* 100);
+    assert(p != NULL);
+    for(int i= 0; i < 100; i++)
+    {
+        p[i]=i;
+    }
+    p[100] = 0;
+
+    //free(p);
+    printf("Dereferenzierungstest ergibt:%d\n",*p);
+    printf("Größe des Pointers:%ld\n",sizeof(p));
+    printf("Größe von Integern:%ld\n",sizeof(int));
+    printf("Größe des dereferenzierten Pointers:%ld\n",sizeof(*p));
+
+    free(p);
+    return 0;
+}
