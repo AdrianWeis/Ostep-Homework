7d20265fa05ab73f0d7a7380fc6810eabe8c1980
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-26 08:17:02 +0200

    with malloc

--- Ch14/null.c
+++ Ch14/null.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <assert.h>
 
 int main()
 {
     int* p;
+    p = malloc(sizeof(int));
+    assert(p != NULL);
     *p = NULL;
     printf("Dereferenzierungstest ergibt:%d",*p);
+    free(p);
+    return 0;
 }
