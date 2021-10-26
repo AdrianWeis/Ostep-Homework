b74729fc90b80bcb98ff7665eb3f4ad40202d32c
AdrianWeis <adrian.weishaupt@htwg-konstanz.de>
2021-10-26 09:01:15 +0200

    14.7

--- Ch14/array.c
+++ Ch14/array.c
@@ -11,16 +11,18 @@ int main()
     {
         p[i]=i;
     }
-    p[100] = 0;
+    //p[100] = 0;
 
-    free(p);
-    printf("Test nach free an index 75:%d\n", p[75]);
+    //free(p);
+    //printf("Test nach free an index 75:%d\n", p[75]);
+
+    free(p[1]);
 
     printf("Dereferenzierungstest ergibt:%d\n",*p);
     printf("Größe des Pointers:%ld\n",sizeof(p));
     printf("Größe von Integern:%ld\n",sizeof(int));
     printf("Größe des dereferenzierten Pointers:%ld\n",sizeof(*p));
 
-    //free(p);
+    free(p);
     return 0;
 }
