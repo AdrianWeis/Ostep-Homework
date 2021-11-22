fe19143727c036c07b179ee701b3b66fda706aae
AdrianWeis <73338420+AdrianWeis@users.noreply.github.com>
2021-11-22 15:42:01 +0100

    Add files via upload

---
+++ Chapter22/txtRand.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+
+# Author: Adrian Weishaupt, Simon Kaemmer
+
+from optparse import OptionParser
+from __future__ import print_function
+from random import randrange
+
+import subprocess
+import os
+import matplotlib.pyplot as plt
+
+trials = 10
+pages = 10
+argument = ""
+arrAdressen = []
+
+parser = OptionParser()
+parser.add_option('-n', '--numaddrs', default=10, help='Number of Address Trials', action='store', type='int', dest='numtrials')
+parser.add_option('-m', '--maxpage', default=10, help='Number of Pages', action='store', type='int', dest='numpages')
+
+(options, args) = parser.parse_args()
+
+trials = options.numtrials
+pages = options.numpages
+
+# Clear whole file to get new values
+open('adrRand.txt', 'w').close()
+
+for i in range(trials):
+    arrAdressen.append(randrange(pages))
+
+argument = ','.join(map(str,arrAdressen))
+
+with open("adr.txt", "w") as text_file:
+    text_file.write(argument)
