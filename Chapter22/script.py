888864aeb0457e1082a2bcb120cbfcd5236a62c1
AdrianWeis <73338420+AdrianWeis@users.noreply.github.com>
2021-11-22 14:26:01 +0100

    Add files via upload

---
+++ Chapter22/script.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+
+# Author: Adrian Weishaupt, Simon Kaemmer
+
+from optparse import OptionParser
+from random import randrange
+
+import subprocess
+
+trials = 10
+pages = 10
+policy = "FIFO"
+argument = ""
+
+parser = OptionParser()
+parser.add_option('-n', '--numaddrs', default=10, help='Number of Address Trials', action='store', type='int', dest='numtrials')
+parser.add_option('-m', '--maxpage', default=10, help='Number of Pages', action='store', type='int', dest='numpages')
+parser.add_option('-p', '--policy', default="FIFO", help='Policy Type', action='store', type='string', dest='policyChoice')
+
+(options, args) = parser.parse_args()
+
+trials = options.numtrials
+pages = options.numpages
+policy = options.policyChoice
+
+arrAdressen[trials]
+
+for i in range(trials)
+    arrAdressen[i] = randrange(pages)
+
+argument = ','.join(map(str,arrAdressen))
+
+subprocess.call(["python3 paging-policy.py", "-p " + policy, "-m " + pages, "-a " + argument, "-c"])
+
+
+
+
