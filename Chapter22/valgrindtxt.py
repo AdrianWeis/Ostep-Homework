3c62ac42bb3e86eba73074885317f23a92ea3f0a
AdrianWeis <73338420+AdrianWeis@users.noreply.github.com>
2021-11-23 10:55:11 +0100

    mit mask und txt

--- Chapter22/valgrindtxt.py
+++ Chapter22/valgrindtxt.py
@@ -4,5 +4,24 @@
 # Author: Adrian Weishaupt, Simon Kaemmer
 
 import subprocess
+import os
+
 
 subprocess.call(["valgrind", "--log-file=val", "--tool=lackey", "--trace-mem=yes" , "ls"])
+
+trace = open("./val", "r")
+adresslist = open("./addressList.txt", "w")
+
+for line in trace:
+    if line.startswith('I'):
+        hex = "0x" + line[3,11]
+        maskedOffset = hex & 0x00000fff
+        maskedPage = hex & 0xfffff000
+        pageShift = maskedPage >> 12
+        adresslist.write(str(int(pageShift)))
+
+trace.close()
+adresslist.close()
+
+
+
