36f6be2b8272d9e0d782b1402b2170e8548c2120
AdrianWeis <73338420+AdrianWeis@users.noreply.github.com>
2021-11-23 12:31:55 +0100

    mit s und l

--- Chapter22/valgrindtxt.py
+++ Chapter22/valgrindtxt.py
@@ -17,10 +17,22 @@ adresslist = open("./addressList.txt", "w")
 for line in trace:
     if line.startswith('I'):
         hex = "0x" + line[3:11]
-        maskedOffset = int(hex,16) & 0x00000fff
         maskedPage = int(hex,16) & 0xfffff000
         pageShift = maskedPage >> 12
-        adresslist.write(str(int(pageShift)) + "\n")
+        adresslist.write("\r\n" + str(int(pageShift)))
+    if line[1] == 'L':
+        hex = "0x" + line[3:11]
+        maskedPage = int(hex,16) & 0xfffff000
+        pageShift = maskedPage >> 12
+        adresslist.write("\r\n" + str(int(pageShift)))
+    if line[1] == 'S':
+        if line[12] != ',':
+            hex = "0x" + line[3:11]
+            maskedPage = int(hex,16) & 0xfffff000
+            pageShift = maskedPage >> 12
+            adresslist.write("\r\n" + str(int(pageShift)))
+        else :
+            #keine Ahnung auf einmal 40 bit
 
 trace.close()
 adresslist.close()
