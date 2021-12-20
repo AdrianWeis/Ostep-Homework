6308842a3b90479e81067e23db14c92c03858141
AdrianWeis <73338420+AdrianWeis@users.noreply.github.com>
2021-12-20 15:44:47 +0100

    test

---
+++ Ch29/test.c
@@ -0,0 +1,19 @@
+#include "c29.h"
+
+typedef struct {
+    int a;
+    int b;
+} myarg_t;
+
+void *mythread(void *arg) {
+    myarg_t *args = (myarg_t *) arg;
+    printf("%d %d\n", args->a, args->b);
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t p;
+    myarg_t args = { 10, 20 };
+    Pthread_create(&p, NULL, mythread, &args);
+    Pthread_join(&p, NULL);
+}
