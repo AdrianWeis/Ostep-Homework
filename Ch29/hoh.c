2f4e054a5d20a770cb7ec9abf37e235e61750220
AdrianWeis <73338420+AdrianWeis@users.noreply.github.com>
2021-12-20 21:44:33 +0100

    Add files via upload

--- Ch29/hoh.c
+++ Ch29/hoh.c
@@ -5,12 +5,12 @@
 typedef struct __node_t {
     int key;
     struct __node_t *next;
+    pthread_mutex_t lock;
 } node_t;
 
 // basic list structure (one used per list)
 typedef struct __list_t {
     node_t *head;
-    pthread_mutex_t lock;
 } list_t;
 
 typedef struct myargs {
@@ -28,38 +28,43 @@ typedef struct { long time; } myret_t;
 
 void List_Init(list_t *L) {
     L->head = NULL;
-    Pthread_mutex_init(&L->lock, NULL);
 }
 
 int List_Insert(list_t *L, int key) {
-    Pthread_mutex_lock(&L->lock);
-    if (List_Lookup(L,key)) {
-        return -1;
-    }
+    
     node_t *new = malloc(sizeof(node_t));
     if (new == NULL) {
         perror("malloc");
-        Pthread_mutex_unlock(&L->lock);
         return -1; // fail
     }
     new->key = key;
+    new->lock = Pthread_mutex_init(new->lock, NULL);
+    
+    if (List_Lookup(L,key)) {
+        return -1;
+    }
+    Pthread_mutex_lock(L->head->lock);
     new->next = L->head;
     L->head = new;
-    Pthread_mutex_unlock(&L->lock);
+    Pthread_mutex_unlock(L->head->lock);
     return 0; // success
 }
 
 int List_Lookup(list_t *L, int key) {
-    Pthread_mutex_lock(&L->lock);
+    Pthread_mutex_lock(L->head->lock);
     node_t *curr = L->head;
+    node_t *tmp;
     while (curr) {
         if (curr->key == key) {
-            Pthread_mutex_unlock(&L->lock);
+            Pthread_mutex_unlock(curr->lock);
             return 0; // success
         }
+        Pthread_mutex_lock(curr->next->lock);
+        tmp = curr;
         curr = curr->next;
+        Pthread_mutex_unlock(tmp->lock);
     }
-    Pthread_mutex_unlock(&L->lock);
+    Pthread_mutex_unlock(curr->lock);
     return -1; // failure
 }
 
